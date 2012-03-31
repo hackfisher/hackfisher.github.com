f6c4d9c34b30597e43ace2dfe000ce014529e0ba
HackFisher <wxiaoyin@gmail.com>
2012-03-31 16:06:34 +0800

    test

---
+++ commit.sh
@@ -0,0 +1,8 @@
+echo 'commit source and checkout master, then deploy'
+git add .
+git commit -a -m $1
+git push origin source
+
+echo 'start checkout master and deploy'
+git checkout master
+sh deploy.sh $1
